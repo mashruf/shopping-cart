5114f96c533684c2b888d0ef407a6b1d67ab8171
mashruf <mashrufrohon@gmail.com>
2022-08-28 02:29:00 +0600

    count the total amount for specific product

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-function productQuantity(product, isAdd) {
+function productQuantity(product, isAdd, productTotal, price) {
     //getting input value to count number of phone and case
     const productInput = document.getElementById(product);
     const productNumber = productInput.value;
+
+    //getting input value to count the total amount for specific product
+    const totalInput = document.getElementById(productTotal);
+
 
     if (isAdd == true) {
         productInput.value = parseInt(productNumber) + 1;
@@ -10,26 +14,24 @@ function productQuantity(product, isAdd) {
         if (productInput.value > 0) {
             productInput.value = parseInt(productNumber) - 1;
         }
-
     }
-
-
+    totalInput.innerText = productInput.value * price;
 }
 
 // phone increase decrease event handler
 document.getElementById('phone-plus').addEventListener('click', function () {
-    productQuantity('phone-input', true);
+    productQuantity('phone-input', true, 'phone-total', 1229);
 });
 
 document.getElementById('phone-minus').addEventListener('click', function () {
-    productQuantity('phone-input', false);
+    productQuantity('phone-input', false, 'phone-total', 1229);
 });
 
 //case increase decrease handler
 document.getElementById('case-plus').addEventListener('click', function () {
-    productQuantity('case-input', true);
+    productQuantity('case-input', true, 'case-total', 59);
 });
 
 document.getElementById('case-minus').addEventListener('click', function () {
-    productQuantity('case-input', false);
+    productQuantity('case-input', false, 'case-total', 59);
 });
